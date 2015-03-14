7d8da8c502de7eb617cd4d6d56e821f4295330fa
CyrusRoshan <CyrusRoshan@users.noreply.github.com>
2015-03-14 01:45:15 -0500

    commit

--- js/url.js
+++ js/url.js
@@ -5,8 +5,10 @@ function url(name) {
 }
 var temp = "";
 window.changeIntro = url("changeIntro");
+window.changeIntro2 = url("changeIntro2");
 window.rawTime = url("time");
 window.category = url("category");
+window.time = "";
 if (rawTime == "oneHr"){
 	time = "one hour or less";
 }
@@ -24,6 +26,30 @@ function introChanger(){
 	if (changeIntro == "true"){
 		$(".introMessage").html("You have " + time +". <br>What category of activity would you like?");
 	}
+	if (changeIntro2 == "true"){
+		var temp = "";
+		//the following changes the output for category types that won't work gramatically when plugged into "List of [category] activities that take [time]."
+		if (category == "academics"){
+			temp = "academic";
+		}
+		else if (category == "diy"){
+			temp = "DIY";
+		}
+		else if (category == "physicalActivity"){
+			temp = "physical";
+		}
+		else if (category == "technology"){
+			temp = "technology related";
+		}
+		else if (category == "misc"){
+			temp = "uncategorized";
+		}
+		else{
+			temp = category;	
+		}
+		$(".introMessage").html("List of " + temp +" activities that take " + time + ":");
+	}
+	//using '== "true"' instead of '=== "true"' because I can't set query string parameter values to true boolean
 }
 
 function titleChanger(){
@@ -44,17 +70,17 @@ function titleChanger(){
 function editTimeLinks(){
 	//var temp = "";
 	//temp = $("#academics").attr("onclick");
-	$("#academics").attr("onclick", "window.location = 'list.html?category=academics&time=" + rawTime + "'");
+	$("#academics").attr("onclick", "window.location = 'list.html?changeIntro2=true&category=academics&time=" + rawTime + "'");
 	//temp = $("#cooking").attr("onclick");
-	$("#cooking").attr("onclick", "window.location = 'list.html?category=cooking&time=" + rawTime + "'");
+	$("#cooking").attr("onclick", "window.location = 'list.html?changeIntro2=true&category=cooking&time=" + rawTime + "'");
 	//temp = $("#diy").attr("onclick");
-	$("#diy").attr("onclick", "window.location = 'list.html?category=diy&time=" + rawTime + "'");
+	$("#diy").attr("onclick", "window.location = 'list.html?changeIntro2=true&category=diy&time=" + rawTime + "'");
 	//temp = $("#physicalActivity").attr("onclick");
-	$("#physicalActivity").attr("onclick", "window.location = 'list.html?category=physicalActivity&time=" + rawTime + "'");
+	$("#physicalActivity").attr("onclick", "window.location = 'list.html?changeIntro2=true&category=physicalActivity&time=" + rawTime + "'");
 	//temp = $("#technology").attr("onclick");
-	$("#technology").attr("onclick", "window.location = 'list.html?category=technology&time=" + rawTime + "'");
+	$("#technology").attr("onclick", "window.location = 'list.html?changeIntro2=true&category=technology&time=" + rawTime + "'");
 	//temp = $("#misc").attr("onclick");
-	$("#misc").attr("onclick", "window.location = 'list.html?category=misc&time=" + rawTime + "'");
+	$("#misc").attr("onclick", "window.location = 'list.html?changeIntro2=true&category=misc&time=" + rawTime + "'");
 }
 
 document.addEventListener('DOMContentLoaded', function () {
